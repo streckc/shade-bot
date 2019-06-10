a394ed23229cbccdf75afe94318f311bb9194902
Chad Streck <cstreck@tenable.com>
2019-06-10 15:05:22 -0500

    ip-api information plugin

---
+++ plugins/ip-api.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+const { say } = require('../bot.js');
+
+const isCapable = () => {
+  try {
+    const installed = require.resolve('axios');
+    if (installed) return true;
+  } catch(err) {
+    return false;
+  }
+  return false;
+}
+
+const execPlugin = async (args, event) => {
+  const response = await axios.get('http://ip-api.com/json');
+  // as, city, country, countryCode, isp, lat, lon, org, query, region,
+  // regionName, status, timesone, zip
+  let text = response.data.query;
+  text += ': ' + response.data.isp;
+  text += ' @ ' + response.data.city;
+  text += ', ' + response.data.region;
+  text += ', ' + response.data.countryCode;
+  say(text, event.channel);
+}
+
+const helpPlugin = async (event) => {
+  return 'Report on information that the bot is located at.';
+}
+
+module.exports = {
+  isCapable,
+  execPlugin,
+  helpPlugin
+}
