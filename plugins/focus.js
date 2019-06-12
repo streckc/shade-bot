dc937ee99eae07eb199226e8aa8ea61e01017aa8
Chad Streck <chad.streck@gmail.com>
2019-06-11 21:56:35 -0500

    add notification of focus

--- plugins/focus.js
+++ plugins/focus.js
@@ -4,14 +4,21 @@ const { removeFromIgnoreList, addToIgnoreList } = require('../lib/commands');
 const isCapable = () => { return true; }
 
 const execPlugin = async (args, event, config, scope) => {
-  if (!scope) scope = [];
+  const all = args;
+  if (scope) {
+    for(let x = 0; x < scope.length; x++) {
+      all.push(scope[x]);
+    }
+  }
 
-  if (scope.length == 0 || scope[0].toLowerCase() == 'all') {
+  if (all.length == 0 || all[0].toLowerCase() == 'all') {
     //remove user to ignore list
     removeFromIgnoreList(event.user);
-  } else if (scope.indexOf(config.hostname) < 0) {
+  } else if (all.indexOf(config.hostname) < 0) {
     //add user to ignore list
     addToIgnoreList(event.user);
+  } else {
+    say('I am listening.', event.channel, config);
   }
 }
 
