03c006feaf0aaefd466467264f4543fb537c21e9
Chad Streck <cstreck@tenable.com>
2019-06-14 09:08:16 -0500

    add internal ip with param

--- plugins/ip.js
+++ plugins/ip.js
@@ -1,3 +1,5 @@
+var os = require('os');
+
 const { say } = require('../lib/slack');
 
 const axios = require('axios');
@@ -12,7 +14,7 @@ const isCapable = () => {
   return false;
 }
 
-const execPlugin = async (args, event, config) => {
+const getExternalAddress = async () => {
   const response = await axios.get('http://ip-api.com/json?fields=59350');
   // as, city, country, countryCode, isp, lat, lon, org, query, region,
   // regionName, status, timesone, zip
@@ -21,11 +23,53 @@ const execPlugin = async (args, event, config) => {
   text += ' @ ' + response.data.city;
   text += ', ' + response.data.region;
   text += ', ' + response.data.countryCode;
-  say(text, event.channel, config);
+
+  return text;
+}
+
+const getInternalAddresses = async () => {
+  const ifaces = os.networkInterfaces();
+  const results = [];
+
+  Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+
+    ifaces[ifname].forEach((iface) => {
+      if ('IPv4' !== iface.family || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        return;
+      }
+
+      if (alias >= 1) {
+        // this single interface has multiple ipv4 addresses
+        results.push(iface.address + ': interface ' + ifname + ':' + alias);
+      } else {
+        // this interface has only one ipv4 adress
+        results.push(iface.address + ': interface ' + ifname);
+      }
+      ++alias;
+    });
+  });
+
+  return results;
+}
+
+const execPlugin = async (args, event, config) => {
+  let text = await getExternalAddress();
+  if (args.includes('all')) {
+    const internal = await getInternalAddresses();
+  console.log(internal);
+    for (let x = 0; x < internal.length; x++) {
+      text += '\n' + internal[x];
+    }
+    say('```' + text + '```', event.channel, config);
+  } else {
+    say(text, event.channel, config);
+  }
 }
 
 const helpPlugin = async (event) => {
-  return 'Report on information that the bot is located at.';
+  return 'Report on information that the bot is located at.\n    all - include internal addresses';
 }
 
 module.exports = {
